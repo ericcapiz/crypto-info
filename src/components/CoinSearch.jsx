8f55117ecee585c524c10fe5d30d93f55efd0e50
eric.capiz <eric.capiz@nextechsol.com>
2022-06-02 21:30:04 -0500

    styling added to coin item

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -5,28 +5,29 @@ const CoinSearch = ({ coins }) => {
   const [search, setSearch] = useState("");
 
   return (
-    <div>
-      <div>
-        <h1>Search Crypto</h1>
+    <div className="rounded-div my-4">
+      <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+        <h1 className="text-2xl font-bold my-2">Search Crypto</h1>
         <form>
           <input
+            className="w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl"
             onChange={(e) => setSearch(e.target.value)}
             type="text"
             placeholder="Search....."
           />
         </form>
       </div>
-      <table>
+      <table className="w-full border-collapse text-center">
         <thead>
-          <tr>
+          <tr className="border-b">
             <th></th>
-            <th>#</th>
-            <th>Crypto</th>
+            <th className="px-4">#</th>
+            <th className="text-left">Crypto</th>
             <th></th>
             <th>Price</th>
             <th>24h</th>
-            <th>24h Volume</th>
-            <th>Mkt</th>
+            <th className="hidden md:table-cell">24h Volume</th>
+            <th className="hidden sm:table-cell">Mkt</th>
             <th>Last 7 Days</th>
           </tr>
         </thead>
